816a8565698e76ea1a803294e88b0465707f769f
klkeys <klkeysb@gmail.com>
2016-07-04 20:22:06 -0700

    intermediate adjustments to gpu.jl

--- src/gpu.jl
+++ src/gpu.jl
@@ -1,38 +1,6 @@
 "A shortcut for OpenCL module name."
 const cl = OpenCL
 
-# container type for all GPU variables
-#type PlinkGPUVariables{T <: Float}
-#    df_buff     :: cl.Buffer{T}
-#    x_buff      :: cl.Buffer{Int8}
-#    y_buff      :: cl.Buffer{T}
-##    mask_n      :: DenseVector{Int}
-#    mask_buff   :: cl.Buffer{Int}
-#    device      :: cl.Device
-#    ctx         :: cl.Context
-#    queue       :: cl.CmdQueue
-#    m_buff      :: cl.Buffer{T}
-#    p_buff      :: cl.Buffer{T}
-#    red_buff    :: cl.Buffer{T}
-#    xtyk        :: cl.Kernel
-#    rxtyk       :: cl.Kernel
-#    reset_x     :: cl.Kernel
-#    wg_size     :: Int
-#    y_chunks    :: Int
-#    r_chunks    :: Int
-#    n           :: Int
-#    p           :: Int
-#    p2          :: Int
-#    n32         :: Int32
-#    p32         :: Int32
-#    y_chunks32  :: Int32
-#    blocksize32 :: Int32
-#    wg_size32   :: Int32
-#    y_blocks32  :: Int32
-#    r_length32  :: Int32
-#    genofloat   :: cl.LocalMem{T}
-#end
-
 type PlinkGPUVariables{T <: Float, V <: cl.Buffer}
     df_buff     :: V 
     x_buff      :: cl.Buffer{Int8}
@@ -62,6 +30,7 @@ type PlinkGPUVariables{T <: Float, V <: cl.Buffer}
     r_length32  :: Int32
     genofloat   :: cl.LocalMem{T}
 end
+
 function PlinkGPUVariables{T <: Float}(
     df_buff     :: cl.Buffer{T}, 
     x_buff      :: cl.Buffer{Int8},
@@ -98,21 +67,23 @@ function PlinkGPUVariables{T <: Float}(
     z      :: DenseVector{T},
     x      :: BEDFile{T},
     y      :: DenseVector{T},
-#    kern   :: ASCIIString      = readall(open(ENV["HOME"] * "/.julia/v0.4/PLINK/src/kernels/iht_kernels64.cl")),
     kern   :: ASCIIString      = readall(open(expanduser("/.julia/v0.4/PLINK/src/kernels/iht_kernels64.cl"))),
     mask_n :: DenseVector{Int} = ones(Int, size(y))
 )
+#    n,p         = size(x,2)
+    n,p         = size(x.geno)
     wg_size     = 512
-    y_chunks    = div(x.geno.n, wg_size) + (x.geno.n % wg_size != 0 ? 1 : 0)
+    y_chunks    = div(n, wg_size) + (n % wg_size != 0 ? 1 : 0)
     y_blocks    = div(y_chunks, wg_size) + (y_chunks % wg_size != 0 ? 1 : 0)
-    r_chunks    = div(x.geno.p*y_chunks, wg_size) + ((x.geno.p*y_chunks) % wg_size != 0 ? 1 : 0)
+#    r_chunks    = div(x.geno.p*y_chunks, wg_size) + ((x.geno.p*y_chunks) % wg_size != 0 ? 1 : 0)
+    r_chunks    = div(p*y_chunks, wg_size) + ((p*y_chunks) % wg_size != 0 ? 1 : 0)
     wg_size32   = convert(Int32, wg_size)
-    n32         = convert(Int32, x.geno.n)
-    p32         = convert(Int32, x.geno.p)
+    n32         = convert(Int32, n)
+    p32         = convert(Int32, p)
     y_chunks32  = convert(Int32, y_chunks)
     y_blocks32  = convert(Int32, y_blocks)
     blocksize32 = convert(Int32, x.geno.blocksize)
-    r_length32  = convert(Int32, x.geno.p*y_chunks)
+    r_length32  = convert(Int32, p*y_chunks)
     device      = last(cl.devices(:gpu))
     ctx         = cl.Context(device) :: cl.Context
     queue       = cl.CmdQueue(ctx) :: cl.CmdQueue
@@ -127,12 +98,11 @@ function PlinkGPUVariables{T <: Float}(
     m_buff      = cl.Buffer(T,    ctx, (:r,  :copy), hostbuf = sdata(x.means)) :: cl.Buffer{T} 
     p_buff      = cl.Buffer(T,    ctx, (:r,  :copy), hostbuf = sdata(x.precs)) :: cl.Buffer{T}
     df_buff     = cl.Buffer(T,    ctx, (:rw, :copy), hostbuf = sdata(z)) :: cl.Buffer{T}
-    red_buff    = cl.Buffer(T,    ctx, (:rw), x.geno.p + x.covar.p*y_chunks) :: cl.Buffer{T}
+    red_buff    = cl.Buffer(T,    ctx, (:rw), p*y_chunks) :: cl.Buffer{T}
 #    xty_buff    = cl.Buffer(T,    ctx, (:rw), x.geno.p + x.covar.p)
     mask_buff   = cl.Buffer(Int,  ctx, (:r,  :copy), hostbuf = sdata(mask_n)) :: cl.Buffer{Int}
     genofloat   = cl.LocalMem(T, wg_size)
 
-#    PlinkGPUVariables(df_buff, x_buff, y_buff, mask_n, mask_buff, queue, m_buff, p_buff, red_buff, xtyk, rxtyk, reset_x, wg_size, y_chunks, r_chunks, x.geno.n, x.geno.p, x.covar.p, n32, p32, y_chunks32, blocksize32, wg_size32, y_blocks32, r_length32, genofloat)
     PlinkGPUVariables{T, cl.Buffer{T}}(df_buff, x_buff, y_buff, mask_buff, device, ctx, queue, m_buff, p_buff, red_buff, xtyk, rxtyk, reset_x, wg_size, y_chunks, r_chunks, x.geno.n, x.geno.p, x.covar.p, n32, p32, y_chunks32, blocksize32, wg_size32, y_blocks32, r_length32, genofloat)
 end
 
@@ -159,10 +129,9 @@ function df_x2!{T <: Float}(
     m = x.means[x.geno.p+snp]
     s = x.precs[x.geno.p+snp]
     df[x.geno.p+snp] = zero(T)
-#    @inbounds for case = 1:x.geno.n
-    for case = 1:x.geno.n
+    @inbounds for case = 1:x.geno.n
         if mask_n[case] == 1
-            df[x.geno.p+snp] += y[case] * (x.covar.x[case,snp] - m) * s
+            @fastmath df[x.geno.p+snp] += y[case] * (x.covar.x[case,snp] - m) * s
         end
     end
     return nothing
@@ -180,7 +149,6 @@ function df_x2!{T <: Float}(
     return nothing
 end
 
-"`At_mul_B!()` can also be called with a configured GPU command queue."
 function Base.At_mul_B!{T <: Float}(
     df          :: SharedVector{T},
     df_buff     :: cl.Buffer,
@@ -218,19 +186,41 @@ function Base.At_mul_B!{T <: Float}(
     cl.wait(cl.call(queue, xtyk, (wg_size*y_chunks,p,1), (wg_size,1,1), n32, p32, y_chunks32, blocksize32, wg_size32, x_buff, red_buff, y_buff, m_buff, p_buff, mask_buff, genofloat))
     cl.wait(cl.call(queue, rxtyk, (wg_size,p,1), (wg_size,1,1), n32, y_chunks32, y_blocks32, wg_size32, red_buff, df_buff, genofloat))
     cl.wait(cl.copy!(queue, sdata(df), df_buff))
-#    @inbounds for snp = 1:p2
-#        df_x2!(snp, df, x, y, mask_n)
-#    end
     df_x2!(df, x, y, mask_n)
     return nothing
 end
 
+# wrapper functions for At_mul_B!
+function copy_y!{T <: Float}(v::PlinkGPUVariables{T}, y::SharedVector{T})
+#    cl.wait(cl.copy!(v.queue, v.y_buff, sdata(y)))
+    cl.copy!(v.queue, v.y_buff, sdata(y)) :: cl.NannyEvent
+end
 
-copy_y!{T <: Float}(v::PlinkGPUVariables{T}, y::SharedVector{T}) = cl.wait(cl.copy!(v.queue, v.y_buff, sdata(y)))
-reset_x!{T <: Float}(v::PlinkGPUVariables{T}) = cl.wait(cl.call(v.queue, v.reset_x, (v.wg_size*v.r_chunks,1,1), nothing, v.red_buff, v.r_length32, zero(T)))
-xty!{T <: Float}(v::PlinkGPUVariables{T}) = cl.wait(cl.call(v.queue, v.xtyk, (v.wg_size*v.y_chunks, v.p, 1), (v.wg_size, 1, 1), v.n32, v.p32, v.y_chunks32, v.blocksize32, v.wg_size32, v.x_buff, v.red_buff, v.y_buff, v.m_buff, v.p_buff, v.mask_buff, v.genofloat))
-xty_reduce!{T <: Float}(v::PlinkGPUVariables{T}) = cl.wait(cl.call(v.queue, v.rxtyk, (v.wg_size,v.p,1), (v.wg_size,1,1), v.n32, v.y_chunks32, v.y_blocks32, v.wg_size32, v.red_buff, v.df_buff, v.genofloat))
-copy_xty!{T <: Float}(v::PlinkGPUVariables{T}) = cl.wait(cl.copy!(v.queue, sdata(xty), v.df_buff))
+function reset_x!{T <: Float}(v::PlinkGPUVariables{T})
+#    cl.wait(cl.call(v.queue, v.reset_x, (v.wg_size*v.r_chunks,1,1), nothing, v.red_buff, v.r_length32, zero(T)))
+    cl.call(v.queue, v.reset_x, (v.wg_size*v.r_chunks,1,1), nothing, v.red_buff, v.r_length32, zero(T)) :: cl.Event
+end
+
+function xty!{T <: Float}(v::PlinkGPUVariables{T})
+#    cl.wait(cl.call(v.queue, v.xtyk, (v.wg_size*v.y_chunks, v.p, 1), (v.wg_size, 1, 1), v.n32, v.p32, v.y_chunks32, v.blocksize32, v.wg_size32, v.x_buff, v.red_buff, v.y_buff, v.m_buff, v.p_buff, v.mask_buff, v.genofloat))
+    cl.call(v.queue, v.xtyk, (v.wg_size*v.y_chunks, v.p, 1), (v.wg_size, 1, 1), v.n32, v.p32, v.y_chunks32, v.blocksize32, v.wg_size32, v.x_buff, v.red_buff, v.y_buff, v.m_buff, v.p_buff, v.mask_buff, v.genofloat) :: cl.Event
+end
+
+function xty_reduce!{T <: Float}(v::PlinkGPUVariables{T})
+#    cl.wait(cl.call(v.queue, v.rxtyk, (v.wg_size,v.p,1), (v.wg_size,1,1), v.n32, v.y_chunks32, v.y_blocks32, v.wg_size32, v.red_buff, v.df_buff, v.genofloat))
+    cl.call(v.queue, v.rxtyk, (v.wg_size,v.p,1), (v.wg_size,1,1), v.n32, v.y_chunks32, v.y_blocks32, v.wg_size32, v.red_buff, v.df_buff, v.genofloat) :: cl.Event
+end
+
+function copy_xty!{T <: Float}(xty::DenseVector{T}, v::PlinkGPUVariables{T})
+#    cl.wait(cl.copy!(v.queue, sdata(xty), v.df_buff))
+    cl.copy!(v.queue, sdata(xty), v.df_buff) :: cl.NannyEvent
+end
+
+"""
+    At_mul_B!(xty, x::BEDFile, y, mask_, v)
+
+Compute `x' * y` for a `BEDFile` object `x` using a GPU command queue configured in the `PlinkGPUVariables` object `v`.
+"""
 function Base.At_mul_B!{T <: Float}(
     xty    :: SharedVector{T},
     x      :: BEDFile{T},
@@ -238,75 +228,15 @@ function Base.At_mul_B!{T <: Float}(
     mask_n :: DenseVector{Int},
     v      :: PlinkGPUVariables{T}
 )
-#@show sum(xty[1:x.geno.p])
-#@show sum(y)
-#@show sum(cl.read(v.queue, v.x_buff))
-#@show sumabs2(cl.read(v.queue, v.m_buff))
-#@show sumabs2(cl.read(v.queue, v.p_buff))
-#@show sumabs2(cl.read(v.queue, v.mask_buff))
-    cl.wait(cl.copy!(v.queue, v.y_buff, sdata(y)))
-#@show sum(cl.read(v.queue, v.y_buff))
-#@show sum(cl.read(v.queue, v.red_buff))
+    copy_y!(v, y)
 #   cl.fill!(queue, red_buff, zero(T))    # only works with OpenCL 1.2
-    cl.wait(cl.call(v.queue, v.reset_x, (v.wg_size*v.r_chunks,1,1), nothing, v.red_buff, v.r_length32, zero(T)))
-#@show sum(cl.read(v.queue, v.red_buff))
-    cl.wait(cl.call(v.queue, v.xtyk, (v.wg_size*v.y_chunks, v.p, 1), (v.wg_size, 1, 1), v.n32, v.p32, v.y_chunks32, v.blocksize32, v.wg_size32, v.x_buff, v.red_buff, v.y_buff, v.m_buff, v.p_buff, v.mask_buff, v.genofloat))
-#@show sum(cl.read(v.queue, v.red_buff))
-    cl.wait(cl.call(v.queue, v.rxtyk, (v.wg_size,v.p,1), (v.wg_size,1,1), v.n32, v.y_chunks32, v.y_blocks32, v.wg_size32, v.red_buff, v.df_buff, v.genofloat))
-#@show sum(cl.read(v.queue, v.red_buff))
-    cl.wait(cl.copy!(v.queue, sdata(xty), v.df_buff))
-#@show sum(xty[1:x.geno.p])
-#    @inbounds for snp = 1:v.p2
-#        df_x2!(snp, xty, x, y, mask_n)
-#    end
+    reset_x!(v)
+    xty!(v)
+    xty_reduce!(v)
+    copy_xty!(xty, v)
     df_x2!(xty, x, y, mask_n)
     return nothing
 end
-
-
-#function Base.At_mul_B!{T <: Float}(
-#    df          :: Vector{T},
-#    df_buff     :: cl.Buffer,
-#    x           :: BEDFile{T},
-#    x_buff      :: cl.Buffer,
-#    y           :: Vector{T},
-#    y_buff      :: cl.Buffer,
-#    mask_n      :: DenseVector{Int},
-#    mask_buff   :: cl.Buffer,
-#    queue       :: cl.CmdQueue,
-#    m_buff      :: cl.Buffer,
-#    p_buff      :: cl.Buffer,
-#    red_buff    :: cl.Buffer,
-#    xtyk        :: cl.Kernel,
-#    rxtyk       :: cl.Kernel,
-#    reset_x     :: cl.Kernel,
-#    wg_size     :: Int,
-#    y_chunks    :: Int,
-#    r_chunks    :: Int,
-#    n           :: Int,
-#    p           :: Int,
-#    p2          :: Int,
-#    n32         :: Int32,
-#    p32         :: Int32,
-#    y_chunks32  :: Int32,
-#    blocksize32 :: Int32,
-#    wg_size32   :: Int32,
-#    y_blocks32  :: Int32,
-#    r_lengh32   :: Int32,
-#    genofloat   :: cl.LocalMem
-#)
-#    cl.wait(cl.copy!(queue, y_buff, sdata(y)))
-##   cl.fill!(queue, red_buff, zero(T))    # only works with OpenCL 1.2
-#    cl.wait(cl.call(queue, reset_x, (wg_size*r_chunks,1,1), nothing, red_buff, r_length32, zero(T)))
-#    cl.wait(cl.call(queue, xtyk, (wg_size*y_chunks,p,1), (wg_size,1,1), n32, p32, y_chunks32, blocksize32, wg_size32, x_buff, red_buff, y_buff, m_buff, p_buff, mask_buff, genofloat))
-#    cl.wait(cl.call(queue, rxtyk, (wg_size,p,1), (wg_size,1,1), n32, y_chunks32, y_blocks32, wg_size32, red_buff, df_buff, genofloat))
-#    cl.wait(cl.copy!(queue, sdata(df), df_buff))
-#    @inbounds for snp = 1:p2
-#        df_x2!(snp, df, x.covar.x, r, mask_n)
-#    end
-#    return nothing
-#end
-
 
 
 """
@@ -314,6 +244,19 @@ end
 
 If called with a kernel file, then `At_mul_B()` will attempt to accelerate computations with a GPU.
 """
+function Base.At_mul_B{T <: Float}(
+    x       :: BEDFile{T},
+    y       :: SharedVector{T},
+    kern    :: ASCIIString;
+    pids    :: DenseVector{Int} = procs(),
+    mask_n  :: DenseVector{Int} = ones(Int, size(y))
+)
+    xty = SharedArray(T, size(x,2), init = S -> S[localindexes(S)] = zero(T), pids=pids)
+    v = PlinkGPUVariables(xty, x, y, kern, mask_n) 
+    At_mul_B!(xty, x, y, v) 
+    return xty
+end
+
 #function Base.At_mul_B{T <: Float}(
 #    x           :: BEDFile{T},
 #    y           :: SharedVector{T},
@@ -355,18 +298,6 @@ If called with a kernel file, then `At_mul_B()` will attempt to accelerate compu
 #    return xty
 #end
 
-function Base.At_mul_B{T <: Float}(
-    x       :: BEDFile{T},
-    y       :: SharedVector{T},
-    kern    :: ASCIIString;
-    pids    :: DenseVector{Int} = procs(),
-    mask_n  :: DenseVector{Int} = ones(Int, size(y))
-)
-    xty = SharedArray(T, size(x,2), init = S -> S[localindexes(S)] = zero(T), pids=pids)
-    v = PlinkGPUVariables(xty, x, y, kern, mask_n) 
-    At_mul_B!(xty, x, y, v) 
-    return xty
-end
 
 #function Base.At_mul_B{T <: Float}(
 #    x           :: BEDFile{T},
